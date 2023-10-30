82be920e76db0787ae63c96ba19728e04260e9ce
jaatsiyah <jaatsiyah@student.telkomuniversity.ac.id>
2023-10-30 13:18:32 +0700

    Halaman Menu Kantin + minor change sidebar tenant

--- TUBES/templates/kasir.js
+++ TUBES/templates/kasir.js
@@ -1,23 +1,3 @@
 
 /*Menu Kasir */
-document.getElementById("buttonTambahMenu").addEventListener("click", function () {
-    document.querySelector(".popupAdd").style.display = "flex";
-  });
   
-  document.querySelector(".close").addEventListener("click", function () {
-    document.querySelector(".popupAdd").style.display = "none";
-  });
-  
-  document.getElementById("buttonSunting").addEventListener("click", function () {
-    document.querySelector(".popupSunting").style.display = "flex";
-  });
-  
-  document.querySelector("#closeSunting").addEventListener("click", function () {
-    document.querySelector(".popupSunting").style.display = "none";
-  });
-  document.getElementById("buttonHapus").addEventListener("click", function () {
-    document.querySelector(".popupDelete").style.display = "flex";
-  });
-  document.getElementById("buttonNo").addEventListener("click", function () {
-    document.querySelector(".popupDelete").style.display = "none";
-  });
